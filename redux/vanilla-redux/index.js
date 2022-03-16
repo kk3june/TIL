4b02a3b772b1986166e2aab0d19bef6cc4c792c0
3jun <kk3june@gmail.com>
2022-03-16 23:04:26 +0900

    :sparkles: 16.2.6 리듀서 함수 정의 및 초깃값 설정

--- redux/vanilla-redux/index.js
+++ redux/vanilla-redux/index.js
@@ -12,3 +12,32 @@ const DECREASE = 'DECREASE';
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = (difference) => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
+
+// 초기값 설정
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// 리듀서 함수 정의
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  // action.type 에 따라 다른 작업 처리함
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+  }
+}
