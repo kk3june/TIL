d25f84819bd0a597b5d6b0617699059258fdc7e8
3jun <kk3june@gmail.com>
2022-03-21 12:29:41 +0900

    :recycle: CounterContainer connect 함수코드 refactoring

--- redux/react-redux-tutorial/src/containers/CounterContainer.js
+++ redux/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import { increase, decrease } from '../modules/counter';
 import Counter from '../components/Counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
@@ -7,17 +8,20 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  // 임시함수
-  increase: () => {
-    console.log('increase');
-  },
-  decrease: () => {
-    console.log('decrease');
-  },
-});
+// const mapStateProps = (state) => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   // 임시함수
+//   increase: () => {
+//     console.log('increase');
+//   },
+//   decrease: () => {
+//     console.log('decrease');
+//   },
+// });
 
-export default connect(mapStateProps, mapDispatchToProps)(CounterContainer);
+export default connect((state) => ({ number: state.counter }), {
+  increase,
+  decrease,
+})(CounterContainer);
