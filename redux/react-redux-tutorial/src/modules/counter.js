ec3b50b3393c999d110a60de0830584a5d91bf17
3jun <kk3june@gmail.com>
2022-03-22 19:35:41 +0900

    :recycle: 17.6.1.1 redux-action 사용하여 counter 모듈 refactoring

--- redux/react-redux-tutorial/src/modules/counter.js
+++ redux/react-redux-tutorial/src/modules/counter.js
@@ -1,28 +1,42 @@
+import { createAction, handleAction } from 'redux-actions';
+
 // 액션 타입
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+// redux-action 적용 =>
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기상태와 리듀서 함수
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+// redux-action 적용 =>
+const counter = handleAction(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
 export default counter;
