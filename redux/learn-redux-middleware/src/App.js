f7085ff55835f27cda38353f81d2b2316e0ec068
3jun <kk3june@gmail.com>
2022-03-23 19:07:51 +0900

    :sparkles: 18.1 작업환경 준비 (리덕스 프로젝트)
    counter 모듈, root 리듀서, index.js 파일에 스토어 생성, Counter 컴포넌트 및 컨테이너 컴포넌트 생성, App.js 에 컨테이너 랜더링

--- redux/learn-redux-middleware/src/App.js
+++ redux/learn-redux-middleware/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
 }
 
 export default App;
