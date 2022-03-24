54031589803528e4a5479ce2c715fc810c45171e
3jun <kk3june@gmail.com>
2022-03-24 19:58:17 +0900

    :sparkles: 18.1.1 미들웨어 만들기

--- redux/learn-redux-middleware/src/index.js
+++ redux/learn-redux-middleware/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import loggerMiddleWare from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleWare));
 
 ReactDOM.render(
   <Provider store={store}>
