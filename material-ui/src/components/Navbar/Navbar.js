458182b06a16a665dd9652c8702e2725bc547995
3jun <kk3june@gmail.com>
2022-03-29 20:50:36 +0900

    :sparkles: 2-1. customize navbar using Drawer, icons-material

---
+++ material-ui/src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { mainNavbarItems } from './consts/navbarItem';
+
+const Navbar = () => {
+  const drawerWidth = 220;
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <Divider />
+      <List>
+        {mainNavbarItems.map((text, index) => (
+          <ListItem button key={text.id}>
+            <ListItemIcon>{text.icon}</ListItemIcon>
+            <ListItemText primary={text.label} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Drawer>
+  );
+};
+
+export default Navbar;
---
+++ material-ui/src/components/Navbar/consts/navbarItem.js
@@ -0,0 +1,45 @@
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AllInboxIcon from '@mui/icons-material/AllInbox';
+import StorageIcon from '@mui/icons-material/Storage';
+import PublicIcon from '@mui/icons-material/Public';
+import FunctionsIcon from '@mui/icons-material/Functions';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
+
+export const mainNavbarItems = [
+  {
+    id: 0,
+    icon: <PeopleAltIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+  {
+    id: 1,
+    icon: <AllInboxIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+  {
+    id: 2,
+    icon: <StorageIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+  {
+    id: 3,
+    icon: <PublicIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+  {
+    id: 4,
+    icon: <FunctionsIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+  {
+    id: 5,
+    icon: <PrecisionManufacturingIcon />,
+    label: 'Authentification',
+    router: 'route',
+  },
+];
