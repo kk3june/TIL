cd62cf5fee195c90042c6adbdb8f4774d067f5d9
3jun <kk3june@gmail.com>
2022-04-26 18:11:25 +0900

    :tada: Lesson 4. Starting point

--- material-ui/src/pages/Authentification/Authentification.js
+++ material-ui/src/pages/Authentification/Authentification.js
@@ -2,30 +2,14 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import CommonButton from "../../components/common/CommonButton/CommonButton";
 
-const Authentification = () => {
-  // 왜 이렇게 하면 버튼 기본색이 빨강이었다가 hover 되면 default 값으로 돌아오나?
-  const buttonStyles = {
-    fontSize: 12,
-    fontWeight: 600,
-    backgroundColor: "red",
-    "&:hover": {
-      backgroundColor: "yellow",
-    },
-  };
+const Authentication = () => {
   return (
     <Grid item xs={8}>
-      Authentification
-      <CommonButton size="large" variant="contained" sx={buttonStyles}>
-        Text
-      </CommonButton>
-      <CommonButton size="large" variant="outlined" sx={buttonStyles}>
-        Text2
-      </CommonButton>
-      <CommonButton size="large" variant="outlined">
-        Text3
-      </CommonButton>
+      This is authentication page.
+      <CommonButton variant="contained">Add user</CommonButton>
+      <CommonButton variant="outlined">Web setup</CommonButton>
     </Grid>
   );
 };
 
-export default Authentification;
+export default Authentication;
--- material-ui/src/pages/dashboardTheme.js
+++ material-ui/src/pages/dashboardTheme.js
@@ -1,15 +1,26 @@
-import * as React from "react";
 import { createTheme } from "@mui/material/styles";
 
 export const dashboardTheme = createTheme({
   components: {
-    // name of component
     MuiButton: {
       styleOverrides: {
-        // name of slot
         root: {
-          // some
-          fontSize: "3rem",
+          fontSize: "0.875rem",
+          fontWeight: 600,
+          borderRadius: 8.5,
+          "&.MuiButton-contained": {
+            backgroundColor: "#009be5",
+            "&:hover": {
+              backgroundColor: "#006db3",
+            },
+          },
+          "&.MuiButton-outlined": {
+            color: "#fff",
+            borderColor: "#fff",
+            "&:hover": {
+              backgroundColor: "transparent",
+            },
+          },
         },
       },
     },
