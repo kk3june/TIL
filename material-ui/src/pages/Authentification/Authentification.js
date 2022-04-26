7ad57af4d92d4bc7ed3adbb6ce97fe35318cbe17
3jun <kk3june@gmail.com>
2022-04-26 17:27:19 +0900

    :lipstick: Authentification 페이지에서 사용할 공통 버튼 설정
    이렇게 하면 buttonStyles에서 지정한 색이 기본 색, hover 했을 때 default 색으로 변경됨 (Why?)

--- material-ui/src/pages/Authentification/Authentification.js
+++ material-ui/src/pages/Authentification/Authentification.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import CommonButton from "../../components/common/CommonButton/CommonButton";
 
 const Authentification = () => {
+  // 왜 이렇게 하면 버튼 기본색이 빨강이었다가 hover 되면 default 값으로 돌아오나?
+  const buttonStyles = {
+    fontSize: 12,
+    fontWeight: 600,
+    backgroundColor: "red",
+  };
   return (
-    <Grid item xs={2}>
+    <Grid item xs={8}>
       Authentification
+      <CommonButton size="large" variant="contained" sx={buttonStyles}>
+        Text
+      </CommonButton>
+      <CommonButton>Text2</CommonButton>
     </Grid>
   );
 };
