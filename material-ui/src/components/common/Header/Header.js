42b88acfe9342f8a51eea55109300cfe7394621c
3jun <kk3june@gmail.com>
2022-04-27 23:59:03 +0900

    :construction: styling Header component

--- material-ui/src/components/common/Header/Header.js
+++ material-ui/src/components/common/Header/Header.js
@@ -1,29 +1,72 @@
 import React from "react";
-import { Avatar, Tooltip, Typography, IconButton } from "@mui/material";
+import { Avatar, Tooltip, Typography, IconButton, Box } from "@mui/material";
 import CommonButton from "../CommonButton/CommonButton";
 import NotificationBell from "../NotificationBell/NotificationBell";
 import HelpIcon from "@mui/icons-material/Help";
 
 const Header = ({ title }) => {
-  return (
-    <div>
-      <CommonButton> Go to DOCS</CommonButton>
-      <NotificationBell iconColor="primary" badgeContent={3} />
-      <Avatar
-        alt="Cindy Baker"
-        src="https://mui.com/static/images/avatar/2.jpg"
-      />
+  const headerStyles = {
+    wrapper: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      backgroundColor: "#009be5",
+      padding: "20px",
+    },
+    topRow: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "end",
+      alignItems: "center",
+      marginBottom: "20px",
+      "*": {
+        marginRight: "5px",
+      },
+    },
+    middleRow: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginBottom: "20px",
+      marginLeft: "320px",
+    },
+    link: {
+      fontWeight: 500,
+      color: "rgba(255, 255, 255, 0.7)",
+      "&:hover": {
+        color: "#fff",
+        cursor: "pointer",
+      },
+    },
+    webButton: {
+      marginRight: "5px",
+    },
+  };
 
-      <div>
-        <Typography>{title}</Typography>
-        <CommonButton variant="outlined">Web setup</CommonButton>
-        <Tooltip title="help">
-          <IconButton>
-            <HelpIcon />
-          </IconButton>
-        </Tooltip>
-      </div>
-    </div>
+  return (
+    <Box sx={headerStyles.wrapper}>
+      <Box sx={headerStyles.topRow}>
+        <Typography sx={headerStyles.link}>Go to docs</Typography>
+        <NotificationBell iconColor="white" />
+        <Avatar src="https://mui.com/static/images/avatar/1.jpg" />
+      </Box>
+      <Box sx={headerStyles.middleRow}>
+        <Typography variant="h1" color="white">
+          {title}
+        </Typography>
+        <Box>
+          <CommonButton sx={headerStyles.webButton} variant="outlined">
+            Web setup
+          </CommonButton>
+          <Tooltip title="Help">
+            <IconButton color="white" sx={headerStyles.helpIcon}>
+              <HelpIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- material-ui/src/pages/Authentification/Authentification.js
+++ material-ui/src/pages/Authentification/Authentification.js
@@ -5,7 +5,6 @@ import Header from "../../components/common/Header/Header";
 const Authentication = () => {
   return (
     <Grid item xs={8}>
-      This is authentication page.
       <Header title={"Authentification"} />
     </Grid>
   );
